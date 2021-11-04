a85b11a3c94ad270437cc6cba0bf2ab5539557be
ehsangh7 <[ehsanghochagh@gmail.com]>
2021-11-04 12:14:17 +0330

    just CR  simple

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello, world!"}
-    
+
+
+@app.get("/posts")
+def get_posts():
+    return {"message": "get post"}
+
+
+@app.post("/createposts")
+def create_post(payload: dict = Body(...)):
+    return {"new_post": f"title {payload['title']} content: {payload['content']}"}
