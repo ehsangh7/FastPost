348046c340a5b862f87253c94d8b55600c24af05
Ehsan Ghochagh <ehsanghochagh@gmail.com>
2021-11-16 02:28:30 +0330

    add router and config files

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from psycopg2.extras import RealDictCursor
 import time
 from . import models, schema, utils, database
 from sqlalchemy.orm import Session
+from .routers import post, user
 
 models.Base.metadata.create_all(bind=database.engine)
 
@@ -47,125 +48,13 @@ def find_index_post(id):
         if p['id'] == id:
             return i
 
+
+app.include_router(post.router)
+app.include_router(user.router)
+
 # READ
 @app.get("/")
 async def root():
     return {"message": "Hello, world!"}
 
 
-@app.get("/posts", response_model = List[schema.Post])
-def get_posts(db: Session = Depends(database.get_db)):
-    # cursor.execute(""" SELECT * FROM posts""")
-    # posts = cursor.fetchall()
-
-    posts = db.query(models.Post).all()
-    return posts
-
-@app.get("/posts/{id}", response_model = schema.Post)
-def get_post(id: int, db: Session = Depends(database.get_db)):
-    # cursor.execute("""SELECT * from posts WHERE id = %s """, (str(id),))
-    # post = cursor.fetchone()
-
-    # ORM
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail=f"couldn't find post {id}")
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {"message": f"couldn't find post {id}"}
-    return post
-
-
-
-# CREATE
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model = schema.Post)
-def create_post(post: schema.PostCreate,db: Session = Depends(database.get_db)):
-    # cursor.execute(""" INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
-    # (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-    # conn.commit()
-
-    #ORM
-    # new_post = models.Post(title=post.title, content=post.content, published=post.published)
-    new_post = models.Post(**post.dict())
-
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-
-    return new_post
-
-
-
-# UPDATE
-@app.put("/posts/{id}", response_model = schema.Post)
-def update_post(id: int, post: schema.PostCreate, db: Session = Depends(database.get_db)):
-    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
-    # (post.title, post.content, post.published, str(id)))
-    
-    # updated_post = cursor.fetchone()
-
-    # if updated_post is None:
-    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post {id} does not exists!!!")
-
-    # conn.commit()
-
-    updated_post = db.query(models.Post).filter(models.Post.id == id)
-    
-    if updated_post.first() is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post {id} does not exists!!!")
-    
-    updated_post.update(post.dict(), synchronize_session=False)
-    
-    db.commit()
-
-    return updated_post.first()
-
-
-
-# DELETE 
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(database.get_db)):
-
-    # cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
-    # deleted_post = cursor.fetchone()
-
-    # if deleted_post is None:
-    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post {id} does not exists!!!")
-
-    # conn.commit()
-
-    # ORM
-    post = db.query(models.Post).filter(models.Post.id == id)
-    if post.first() is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post {id} does not exists!!!")
-
-    post.delete(synchronize_session=False)
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model = schema.UserOut)
-def create_user(user: schema.UserCreate,db: Session = Depends(database.get_db)):
-
-    # hash the password - user password
-    hashed_password = utils.hash(user.password)
-    user.password = hashed_password
-    new_user = models.User(**user.dict())
-
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-
-    return new_user
-
-@app.get('/users/{id}', response_model = schema.UserOut)
-def get_user(id: int, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-
-
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with id={id} does not exists!!!")
-
-    return user
