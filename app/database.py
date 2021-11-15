29d1fbac2b2df719f22c62f824322d7ed9e12c20
Ehsan Ghochagh <ehsanghochagh@gmail.com>
2021-11-15 20:07:32 +0330

    modified

--- app/database.py
+++ app/database.py
@@ -3,13 +3,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+from .config import settings
 
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://exsan:ehs@n2404@localhost/fastpost"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://exsan:ehsan2404@localhost/fastpost"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
