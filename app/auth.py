981921f242bb04d729839cd9739344a0e044709b
Ehsan Ghochagh <ehsanghochagh@gmail.com>
2021-11-17 17:22:59 +0330

    create form data for user creadentials

--- app/auth.py
+++ app/auth.py
@@ -1,15 +1,21 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 from . import models, schema, utils, database, oauth2
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter(
     tags=['Authentication']
 )
 
 @router.post('/login')
-def login(user_credentials: schema.UserLogin ,db: Session = Depends(database.get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends() ,db: Session = Depends(database.get_db)):
 
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+    {
+        "username": "asdf",
+        "password": "asdfas"
+    }
+
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"invalid Credentials")
