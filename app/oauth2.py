82ec61b1d9cba28a758dd743d14fedc1c1d6d8fe
ehsangh7 <ehsanghochagh@gmail.com>
2021-11-20 18:02:55 +0330

    auth current user

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,11 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from . import schema, database, models
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 # SECRET KEY
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 
@@ -12,10 +18,42 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data: dict):
     to_encode = data.copy()
     
-    expire = datetime.now() + timedelta(seconds=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
     encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
    
     return encode_jwt
+
+def verify_access_token(token: str, creadentials_exception):
+
+    try:
+        print(token)
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        print(payload)
+        id: str = payload.get("user_id")
+        print(id)
+        if id is None:
+            print(2)
+            raise creadentials_exception
+        print(3)
+        token_data = schema.TokenData(id=id)
+        print("data",token_data)
+
+    except JWTError:
+        raise creadentials_exception
+
+    return token_data
+
+def get_current_user(token: str = Depends(oauth2_scheme),db: Session =  Depends(database.get_db)):
+    print(222)
+    creadentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                                            detail=f"Could not validate creadentials", 
+                                            headers={"WWW-Authenticate": "Bearer"})
+
+    token = verify_access_token(token, creadentials_exception)
+
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+    print(user)
+    return user 
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,4 +1,4 @@
-from .. import models, schema, utils, database
+from .. import models, schema, utils, database, oauth2
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from typing import Optional, List
@@ -18,11 +18,12 @@ def get_posts(db: Session = Depends(database.get_db)):
     return posts
 
 @router.get("/{id}", response_model = schema.Post)
-def get_post(id: int, db: Session = Depends(database.get_db)):
+def get_post(id: int, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * from posts WHERE id = %s """, (str(id),))
     # post = cursor.fetchone()
 
     # ORM
+    print("userId",current_user)
     post = db.query(models.Post).filter(models.Post.id == id).first()
 
     if not post:
@@ -36,7 +37,7 @@ def get_post(id: int, db: Session = Depends(database.get_db)):
 
 # CREATE
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model = schema.Post)
-def create_post(post: schema.PostCreate,db: Session = Depends(database.get_db)):
+def create_post(post: schema.PostCreate,db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
     # (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
@@ -44,9 +45,11 @@ def create_post(post: schema.PostCreate,db: Session = Depends(database.get_db)):
 
     #ORM
     # new_post = models.Post(title=post.title, content=post.content, published=post.published)
+    
+    print(current_user.email)
     new_post = models.Post(**post.dict())
 
-    db.add(new_post)
+    db.add(new_post) 
     db.commit()
     db.refresh(new_post)
 
@@ -82,7 +85,7 @@ def update_post(id: int, post: schema.PostCreate, db: Session = Depends(database
 
 # DELETE 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(database.get_db)):
+def delete_post(id: int, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
 
     # cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
     # deleted_post = cursor.fetchone()
