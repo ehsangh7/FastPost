a0235ed2fdf12585d545b1a9a13d38f76d73d268
ehsangh7 <ehsanghochagh@gmail.com>
2021-11-22 23:23:09 +0330

    Exception

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -12,6 +12,10 @@ def vote(vote: schema.Vote,
         db: Session = Depends(database.get_db), 
         current_user: int = Depends(oauth2.get_current_user)):
 
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {vote.post_id} does not exist")
+
     print("Vote")
     vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id)
     found_vote = vote_query.first()
