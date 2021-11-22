9acb67834cb33c0fc48567433c21386ce59d0fc9
ehsangh7 <ehsanghochagh@gmail.com>
2021-11-22 22:09:23 +0330

    add voting progress

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 
 from fastapi import FastAPI
 from . import models, database
-from .routers import post, user, auth
+from .routers import post, user, auth, vote
 
 models.Base.metadata.create_all(bind=database.engine)
 
@@ -11,6 +11,8 @@ app = FastAPI()
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
+
 
 # READ
 @app.get("/")
--- app/schema.py
+++ app/schema.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from pydantic import BaseModel, EmailStr
 from typing import  Optional
+from pydantic.types import conint
 
 # Pydantic Schema Model
 
@@ -45,3 +46,7 @@ class Token(BaseModel):
 class TokenData(BaseModel):
     id: Optional[str] = None
 
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1)
