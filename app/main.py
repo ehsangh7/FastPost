767e6618fff3f7a13394cfcaf5fff453c591d611
ehsangh7 <ehsanghochagh@gmail.com>
2021-11-23 16:50:37 +0330

    add CORS Credentials

--- app/main.py
+++ app/main.py
@@ -2,12 +2,28 @@
 from fastapi import FastAPI
 from . import models, database
 from .routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 # with alembic dont need it
 # models.Base.metadata.create_all(bind=database.engine)
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
