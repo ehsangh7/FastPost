63ff2be04303ea5cf81ca34cd4493c3164750c14
ehsangh7 <ehsanghochagh@gmail.com>
2021-11-23 18:36:24 +0330

    modified

--- app/database.py
+++ app/database.py
@@ -8,7 +8,9 @@ from .config import settings
 
 
 SQLALCHEMY_DATABASE_URL = settings.database_url
-
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
